2ac37d75c4421dd5a0a2c41fae5d73eaa644b67c
YuDzhi <ruthhieris@gmail.com>
2018-07-30 07:03:35 +0300

    smth crzfl

--- YuDzhi.FCND_Motion_Planning/motion_planning.py
+++ YuDzhi.FCND_Motion_Planning/motion_planning.py
@@ -11,13 +11,13 @@ from planning_utils import eucl_heuristic, manh_heuristic
 from planning_utils import find_start_goal_skeleton
 from planning_utils import create_graph
 from udacidrone import Drone
-from udacidrone.connection import MavlinkConnection
+from udacidrone.connection import CrazyflieConnection #MavlinkConnection
 from udacidrone.messaging import MsgID
 import networkx as nx
 from skimage.morphology import medial_axis
 from skimage.util import invert
 #from udacidrone.frame_utils import global_to_local
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
 class States(Enum):
     MANUAL = auto()
@@ -52,26 +52,41 @@ class MotionPlanning(Drone):
         self.register_callback(MsgID.STATE, self.state_callback)
 
     def local_position_callback(self):
+        if self.flight_state == States.MANUAL:
+            self.plan_path()
+        elif self.flight_state == States.PLANNING:
+            self.takeoff_transition()
         if self.flight_state == States.TAKEOFF:
-            if len(self.waypoints) < 1.0:
-                print("didn't make it")
+            try:
+                if len(self.waypoints) < 1.0:
+                    print("didn't make it")
+                    self.landing_transition()
+                elif -1.0 * self.local_position[2] > 0.95 * self.target_position[2]:
+                    self.waypoint_transition()
+            except TypeError:
+                print(self.waypoints, type(self.waypoints))
                 self.landing_transition()
-            elif -1.0 * self.local_position[2] > 0.95 * self.target_position[2]:
-                self.waypoint_transition()
+#            else:
+#                self.landing_transition()
         elif self.flight_state == States.WAYPOINT:
             #print('lenwpt',len(self.waypoints), 'targ',self.target_position,'loc',self.local_position)
-            if np.linalg.norm(self.target_position[0:2] - self.local_position[0:2]) < 1.0:
+            if np.linalg.norm(self.target_position[0:2] - self.local_position[0:2]) < 0.2:
                 if len(self.waypoints) > 0:
                     self.waypoint_transition()
                 else:
-                    if np.linalg.norm(self.local_velocity[0:2]) < 1.0:
+                    if np.linalg.norm(self.local_velocity[0:2]) < 0.5:
                         self.landing_transition()
 
     def velocity_callback(self):
         if self.flight_state == States.LANDING:
-            if self.global_position[2] - self.global_home[2] < 0.1:
-                if abs(self.local_position[2]) < 0.01:
-                    self.disarming_transition()
+            if abs(self.local_position[2] < 0.01):
+                self.manual_transition()
+
+#    def velocity_callback(self):
+#        if self.flight_state == States.LANDING:
+#            if self.global_position[2] - self.global_home[2] < 0.1:
+#                if abs(self.local_position[2]) < 0.01:
+#                    self.disarming_transition()
 
     def state_callback(self):
         if self.in_mission:
@@ -144,66 +159,85 @@ class MotionPlanning(Drone):
                           #{1:lambda s: np.char.strip(np.compat.asstr(s),',')})
         return lon0, lat0, alt0
 
+    def calculate_box(self):
+        cp = self.local_position
+        cp[2] = 0
+        local_waypoints = [cp + [0.5, 0.0, 0.5], cp + [0.5, 0.5, 0.5], 
+                           cp + [0.0, 0.5, 0.5], cp + [0.0, 0.0, 0.5]]
+        return local_waypoints
+    
     def plan_path(self):
         self.flight_state = States.PLANNING
         print("Searching for a path ...")
-        TARGET_ALTITUDE = 10
+        TARGET_ALTITUDE = 0.5
         SAFETY_DISTANCE = 3
-        filename = 'colliders.csv'
         self.target_position[2] = TARGET_ALTITUDE
-        #VORONOI, MEDIAL_AXIS, PROBABILISTIC
-        self.search_alg = SearchAlg.VORONOI
-        # TODO: set home position to (lon0, lat0, 0)
-        lon0, lat0, alt0 = self.read_home_position(filename)
-        self.set_home_position(lon0, lat0, alt0)        
-
-        # TODO: retrieve current global position
-        #cur_gl_lon, cur_gl_lat, cur_gl_alt = self.global_position()
-        # TODO: convert to current local position using global_to_local()
-        local_position = global_to_local(self.global_position, self.global_home)
-        
-        print('global home {0}, position {1}, local position {2}'.format(self.global_home, self.global_position,
-                                                                         self.local_position))
-          
-        # Read in obstacle map
-        data = np.loadtxt(filename, delimiter=',', dtype='Float64', skiprows=2)
-
-        # Define starting point on the grid (this is just grid center)
-        #grid_start = (-north_offset, -east_offset)
-        # TODO: convert start position to current position rather than map center
-        
-        start_ne = [(local_position[0],local_position[1], TARGET_ALTITUDE)]
-        #start_ne = Sampler.random_sample(data, flight_altitude, 1)
-        # Set goal as some arbitrary position on the grid
-        #grid_goal = (-north_offset + 10, -east_offset + 10)
-        #Define a start and goal location   
-        
-        # TODO: adapt to set goal as latitude / longitude position and convert
-        #Modify this to be set as some arbitrary position on the grid given 
-        #any geodetic coordinates (latitude, longitude)
-        #goal_ne = [(start_ne[0][0]+30,start_ne[0][1]+1,start_ne[0][2])]
-        goal_ne = Sampler.random_sample(data, TARGET_ALTITUDE, 1,False)
-        #print("RandomStart", start_ne, "RandomGoal", goal_ne)
-        
-        
-        #place random start and goal points on graph
-        north_min = Sampler.datalimits(data)[0]
-        east_min = Sampler.datalimits(data)[2]
-        #print('stne', start_ne, 'nmin',north_min, 'e',east_min,'gne',goal_ne)
-        start_v = (start_ne[0][0] - north_min, start_ne[0][1] - east_min, start_ne[0][2])
-        goal_v = (goal_ne[0][0] - north_min, goal_ne[0][1] - east_min, goal_ne[0][2])
-        print("Start on plot {0}, Goal on plot {1}".format(start_v, goal_v))
-        waypoints = self.find_path(data, TARGET_ALTITUDE, SAFETY_DISTANCE, 
-                              start_v, goal_v, north_min, east_min)
-        # Convert path to waypoints
-        print("North min = {0}, east min = {1}".format(north_min, east_min))
-
-        print('len_wpt', len(waypoints), waypoints[0:2])
-
         # Set self.waypoints
-        self.waypoints = waypoints
+        self.waypoints = self.choose_path_source(self.calculate_box(), TARGET_ALTITUDE, SAFETY_DISTANCE) #'colliders.csv'
         # TODO: send waypoints to sim (this is just for visualization of waypoints)
-        self.send_waypoints()
+        #self.send_waypoints()
+        
+    def choose_path_source(self,path_source, TARGET_ALTITUDE, SAFETY_DISTANCE):
+        if isinstance(path_source, str):
+            print('str')
+            filename = path_source
+
+            #VORONOI, MEDIAL_AXIS, PROBABILISTIC
+            self.search_alg = SearchAlg.VORONOI
+            # TODO: set home position to (lon0, lat0, 0)
+            lon0, lat0, alt0 = self.read_home_position(filename)
+            self.set_home_position(lon0, lat0, alt0)        
+    
+            # TODO: retrieve current global position
+            #cur_gl_lon, cur_gl_lat, cur_gl_alt = self.global_position()
+            # TODO: convert to current local position using global_to_local()
+            local_position = global_to_local(self.global_position, self.global_home)
+            
+            print('global home {0}, position {1}, local position {2}'.format(self.global_home, self.global_position,
+                                                                             self.local_position))
+              
+            # Read in obstacle map
+            data = np.loadtxt(filename, delimiter=',', dtype='Float64', skiprows=2)
+    
+            # Define starting point on the grid (this is just grid center)
+            #grid_start = (-north_offset, -east_offset)
+            # TODO: convert start position to current position rather than map center
+            
+            start_ne = [(local_position[0],local_position[1], TARGET_ALTITUDE)]
+            #start_ne = Sampler.random_sample(data, flight_altitude, 1)
+            # Set goal as some arbitrary position on the grid
+            #grid_goal = (-north_offset + 10, -east_offset + 10)
+            #Define a start and goal location   
+            
+            # TODO: adapt to set goal as latitude / longitude position and convert
+            #Modify this to be set as some arbitrary position on the grid given 
+            #any geodetic coordinates (latitude, longitude)
+            #goal_ne = [(start_ne[0][0]+30,start_ne[0][1]+1,start_ne[0][2])]
+            goal_ne = Sampler.random_sample(data, TARGET_ALTITUDE, 1,False)
+            #print("RandomStart", start_ne, "RandomGoal", goal_ne)
+            
+            
+            #place random start and goal points on graph
+            north_min = Sampler.datalimits(data)[0]
+            east_min = Sampler.datalimits(data)[2]
+            #print('stne', start_ne, 'nmin',north_min, 'e',east_min,'gne',goal_ne)
+            start_v = (start_ne[0][0] - north_min, start_ne[0][1] - east_min, start_ne[0][2])
+            goal_v = (goal_ne[0][0] - north_min, goal_ne[0][1] - east_min, goal_ne[0][2])
+            print("Start on plot {0}, Goal on plot {1}".format(start_v, goal_v))
+            waypoints = self.find_path(data, TARGET_ALTITUDE, SAFETY_DISTANCE, 
+                                  start_v, goal_v, north_min, east_min)
+            # Convert path to waypoints
+            print("North min = {0}, east min = {1}".format(north_min, east_min))
+    
+            print('len_wpt', len(waypoints), waypoints[0:2])
+        else:
+            print('boxcalc')
+            waypoints = path_source
+            for j in range(len(waypoints)):
+                waypoints[j] = [waypoints[j][0], waypoints[j][1], waypoints[j][2], 0]
+            print(len(waypoints),waypoints)
+            
+        return waypoints
 
 
     def find_path(self, data, TARGET_ALTITUDE, SAFETY_DISTANCE, start_v, goal_v, nmin, emin):
@@ -359,7 +393,8 @@ if __name__ == "__main__":
     parser.add_argument('--host', type=str, default='127.0.0.1', help="host address, i.e. '127.0.0.1'")
     args = parser.parse_args()
 
-    conn = MavlinkConnection('tcp:{0}:{1}'.format(args.host, args.port), timeout=60)
+    #conn = MavlinkConnection('tcp:{0}:{1}'.format(args.host, args.port), timeout=60)
+    conn = CrazyflieConnection('radio://0/80/2M')
     drone = MotionPlanning(conn)
     time.sleep(2)
 
